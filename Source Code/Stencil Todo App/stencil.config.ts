6acbefccdd667d053ec92c82d040b25d0cc5afee
Amey Thakur <ameythakur20@gmail.com>
2026-02-02 15:51:42 -0500

    JS Framework
    Co-authored-by: Mega Satish <mega.modha@gmail.com>

--- Source Code/Stencil Todo App/stencil.config.ts
+++ Source Code/Stencil Todo App/stencil.config.ts
@@ -12,7 +12,7 @@ export const config: Config = {
     },
     {
       type: 'www',
-      baseUrl: '/JAVASCRIPT-FRAMEWORKS-TODO-APPS/Stencil%20Todo%20App/',
+      baseUrl: '/JAVASCRIPT-FRAMEWORKS-TODO-APPS/Stencil-Todo-App/',
       serviceWorker: null, // disable service workers
       copy: [
         { src: 'assets' }
